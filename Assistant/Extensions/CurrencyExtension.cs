20fef4c2946dbc3df2b28520838eab90ca803782
siamak <siamakjalilii@gmail.com>
2023-12-24 10:10:12 +0330

    Update CurrencyExtension.cs

--- Assistant/Extensions/CurrencyExtension.cs
+++ Assistant/Extensions/CurrencyExtension.cs
@@ -10,27 +10,49 @@ namespace PersianAssistant.Extensions
         {
             return value.ToString("#,0 ریال");
         }
-
         public static string Rial(this double value)
         {
             return value.ToString("#,0 ریال");
         }
-
         public static string Rial(this int value)
         {
             return value.ToString("#,0 ریال");
+        }
+        public static string Rial(this decimal? value)
+        {
+            return !value.HasValue ? 0.ToString("#,0 ریال") : value.Value.ToString("#,0 ریال");
+        }
+        public static string Rial(this double? value)
+        {
+            return !value.HasValue ? 0.ToString("#,0 ریال") : value.Value.ToString("#,0 ریال");
+        }
+        public static string Rial(this int? value)
+        {
+            return !value.HasValue ? 0.ToString("#,0 ریال") : value.Value.ToString("#,0 ریال");
         }
         public static string Toman(this float value)
         {
             return value.ToString("#,0 تومان");
         }
+        public static string Toman(this int value)
+        {
+            return value.ToString("#,0 تومان");
+        }
+        public static string Toman(this double value)
+        {
+            return value.ToString("#,0 تومان");
+        }
+        public static string Toman(this float? value)
+        {
+            return !value.HasValue ? 0.ToString("#,0 تومان") : value.Value.ToString("#,0 تومان");
+        }
         public static string Toman(this int? value)
         {
-            return value == null ? 0.ToString("#,0 تومان") : value.Value.ToString("#,0 تومان");
+            return !value.HasValue ? 0.ToString("#,0 تومان") : value.Value.ToString("#,0 تومان");
         }
         public static string Toman(this double? value)
         {
-            return value == null ? 0.ToString("#,0 تومان") : value.Value.ToString("#,0 تومان");
+            return !value.HasValue ? 0.ToString("#,0 تومان") : value.Value.ToString("#,0 تومان");
         }
     }
 }
